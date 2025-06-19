3aefce91ef307d0747b7d3dc9a1200f7b137342f
kevin-71 <kevin.jabiol@efrei.net>
2025-06-19 20:50:35 +0200

    put everything in the main to run only this file

--- scripts/main.py
+++ scripts/main.py
@@ -2,15 +2,19 @@ import scraper_rss
 import extract_cve
 import enrich_cve
 import consolidate_df
+import model_py
 
-print("le scraping commence !")
-scraper_rss.main()
+#print("le scraping commence !")
+#scraper_rss.main()
+#
+#print("l'extraction des CVE commence")
+#extract_cve.main()
+#
+#print("l'enrichissement des CVE commence")
+#enrich_cve.main()
+#
+#print("consolidation du csv et df")
+#consolidate_df.main()
 
-print("l'extraction des CVE commence")
-extract_cve.main()
-
-print("l'enrichissement des CVE commence")
-enrich_cve.main()
-
-print("consolidation du csv et df")
-consolidate_df.main()
+print("envoi des emails")
+model_py.main()
