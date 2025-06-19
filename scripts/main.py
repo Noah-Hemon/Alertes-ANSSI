421253252ddb53034de22c8439be11270c8c17d7
kevin-71 <kevin.jabiol@efrei.net>
2025-06-19 20:52:00 +0200

    removed comment for test

--- scripts/main.py
+++ scripts/main.py
@@ -4,17 +4,17 @@ import enrich_cve
 import consolidate_df
 import model_py
 
-#print("le scraping commence !")
-#scraper_rss.main()
-#
-#print("l'extraction des CVE commence")
-#extract_cve.main()
-#
-#print("l'enrichissement des CVE commence")
-#enrich_cve.main()
-#
-#print("consolidation du csv et df")
-#consolidate_df.main()
+print("le scraping commence !")
+scraper_rss.main()
+
+print("l'extraction des CVE commence")
+extract_cve.main()
+
+print("l'enrichissement des CVE commence")
+enrich_cve.main()
+
+print("consolidation du csv et df")
+consolidate_df.main()
 
 print("envoi des emails")
 model_py.main()
